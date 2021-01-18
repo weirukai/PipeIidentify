36c32bfdf3434b1eceb1e6e13e9daf9ab55907de
weirukai <15623739350@163.com>
2021-01-18 16:19:05 +0800

    文件生成

---
+++ code/Logger.h
@@ -0,0 +1,14 @@
+//
+// Created by 魏 on 2021/1/18.
+//
+
+#ifndef PIPEIDENTIFY_LOGGER_H
+#define PIPEIDENTIFY_LOGGER_H
+
+
+class Logger {
+
+};
+
+
+#endif //PIPEIDENTIFY_LOGGER_H
---
+++ code/PipeProcessor.h
@@ -0,0 +1,14 @@
+//
+// Created by 魏 on 2021/1/18.
+//
+
+#ifndef PIPEIDENTIFY_PIPEPROCESSOR_H
+#define PIPEIDENTIFY_PIPEPROCESSOR_H
+
+
+class PipeProcessor {
+
+};
+
+
+#endif //PIPEIDENTIFY_PIPEPROCESSOR_H
